5d7f5701122d64ca9c3b07f1f987448e06764dc7
Shafiqur Rahman <pallab.alif@gmail.com>
2022-11-27 13:14:04 +0600

    TEST: Is Github Error Fixed?

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,7 +78,8 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
